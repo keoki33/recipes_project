d1e72c1d204340534e7c70a4cd12b0eb5f5dc1aa
keoki33 <gwong323@yahoo.com>
2019-05-28 08:40:11 +0100

    like button behaviour

--- src/RecipeCard.js
+++ src/RecipeCard.js
@@ -6,9 +6,17 @@ import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import { Card, Icon, Image } from "semantic-ui-react";
 
 class RecipeCard extends Component {
-  state = {};
+  state = { favourite: false };
 
   whatever = () => {};
+
+  toggleFavorite = () => {
+    return this.state.favourite ? (
+      <Icon name="heart" style={{ color: "red" }} />
+    ) : (
+      <Icon name="heart" />
+    );
+  };
 
   render() {
     return (
@@ -29,9 +37,16 @@ class RecipeCard extends Component {
               </Card.Description>
             </Card.Content>
             <Card.Content extra>
-              <button>
-                {<Icon name="heart" />}
-                Add to Favorites
+              <button
+                onClick={event => {
+                  event.stopPropagation();
+                  this.state.favourite
+                    ? this.setState({ favourite: false })
+                    : this.setState({ favourite: true });
+                }}
+              >
+                {this.toggleFavorite()}
+                {this.state.favourite === false && "Add to Favourites"}
               </button>
             </Card.Content>
           </Card>
